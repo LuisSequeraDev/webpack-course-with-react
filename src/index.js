58f8776a7228e8aeceda3febb1adac0856cc21da
Luis Sequera <luissequeracontreras@gmail.com>
2022-08-25 16:31:34 -0500

    lastest config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-import ReactDom from "react-dom/client";
+import ReactDom from "react-dom";
 import App from "./components/App";
+import './styles/global.scss';
 
 const root = ReactDom.createRoot(document.getElementById("app"));
+render(<App />, root);
